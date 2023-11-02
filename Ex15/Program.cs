5cfb2e75f5f9ff4f3e7c275ccb22c8fdab21f473
Moe <35382301+Moewagon@users.noreply.github.com>
2023-11-02 20:47:49 +0100

    Ex 15/16

---
+++ Ex15/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.WriteLine("Escriu un numero");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escriu el segon numero");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (a<b)
+            {
+                Console.WriteLine($"{a},{b}");
+            }
+            else
+            {
+                c = b;
+                b = a;
+                a = c;
+                Console.WriteLine($"{a},{b}");
+            }
+        }
+    }
+}
+/*Fes un programa que demani dos números qualssevol i després els mostri en ordre creixent.
+Demana a i b, i escriu a i b. Ha de permutar el valor de les variables si cal*/
---
+++ Ex16/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ex16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            Console.WriteLine("Entra el primer numero");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entra el segon numero");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entra el tercer numero");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            if (x <= y && y <= z) 
+            {
+                Console.WriteLine("Els numeros estan en ordre ascendent");
+            }
+            else if (x >= y && y >= z) 
+            {
+                Console.WriteLine("Els numeros estan en ordre descendent");
+            }
+            else
+            {
+                Console.WriteLine("Els numeros no estan ordenats");
+            }
+        }
+    }
+}
+/*Fes un programa que demani tres números introduïts per teclat o digui si estan ordenats. (dos
+nombres entrats consecutivament iguals considerarem que no trenquen l’ordre).*/
