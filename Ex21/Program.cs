b7de90fd941b1d2b895a18052a7a3d0291d50017
Moe <35382301+Moewagon@users.noreply.github.com>
2023-11-08 01:30:36 +0100

    Update Program.cs

--- Ex21/Program.cs
+++ Ex21/Program.cs
@@ -8,3 +8,7 @@
         }
     }
 }
+
+/*Fes un programa que donada una hora, minut i segon, incrementi un segon i mostri l’hora
+resultant. Cal verificar que l’hora estigui entre 0 i 23, els minuts entre 0 i 59 i els segons
+entre 0 i 59.*/
